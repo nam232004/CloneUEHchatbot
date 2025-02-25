43e2dc8813689787c15c6ecc4064008f30a1c257
NamBiLapTrinh <namdev232004@gmail.com>
2025-02-25 22:27:19 +0700

    fix same same hehe

--- src/Pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { NavHome } from "../Components/Layout/NavHome";
-
-const Home: React.FC = () => {
-  return <NavHome />;
-};
-
-export default Home;
--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,6 +1,5 @@
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "../Pages/Home";
 import { Layout } from "../Components/Layout/Layout";
 import ChatPage from "../Pages/ChatPage";
 
@@ -9,19 +8,21 @@ const router = createBrowserRouter([
         path: "/",
         element: <Layout />,
         children: [
-            { index: true, element: <Home /> },
+            { index: true, element: <ChatPage /> },
             { path: "chat", element: <ChatPage /> },
-            {
-                path: "*",
-                element: (
-                    <div className="mt-10 text-xl text-center text-red-500">
-                        404 - Trang không tồn tại
-                    </div>
-                ),
-            },
         ],
     },
+    {
+        path: "*",
+        element: (
+            <div className="mt-10 text-xl text-center text-red-500">
+                404 - Trang không tồn tại
+            </div>
+        ),
+    },
 ]);
+
+
 
 const AppRouter = () => {
     return <RouterProvider router={router} />;
