5796ed0a4458be6f645e6442d18c5f0645d1783d
NamBiLapTrinh <namdev232004@gmail.com>
2025-02-26 21:36:18 +0700

    fix btn

--- src/Components/Chat/ChatSideBar.tsx
+++ src/Components/Chat/ChatSideBar.tsx
@@ -52,7 +52,7 @@ export const ChatSideBar: React.FC = () => {
                                     flex-1 md:flex-none transition-all duration-300 p-2 bg-primary text-white
                                     ${isSidebarReponsive && window.innerWidth < 768 ?
                                     'py-3 text-center text-white rounded bg-primary flex items-center justify-center' :
-                                    'md:rounded-full md:w-5 md:h-5 md:flex md:items-center md:justify-center '
+                                    'md:rounded-full md:flex md:items-center md:justify-center '
                                 }
                                 `}
                         >
@@ -60,7 +60,7 @@ export const ChatSideBar: React.FC = () => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 16 16"
                                 fill="currentColor"
-                                className="hidden md:block w-10 h-10"
+                                className="hidden md:block size-2"
                             >
                                 <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
                             </svg>
